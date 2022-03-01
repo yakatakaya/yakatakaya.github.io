80d216509f94a9940427d3de28b7f87ebabfd0d9
yakata <tagat92tagat@gmail.com>
2022-03-01 23:07:04 +0900

    mermaid.js sono5kurai

--- assets/js/mermaid.js
+++ assets/js/mermaid.js
@@ -1,21 +1,5 @@
-const mermaiding = function() {
-    const elements = document.querySelectorAll("pre>code.language-mermaid");
-    for (let i = 0; i < elements.length; i++) {
-        const e = elements[i];
-        const pre = e.parentElement;
-        const replace = function(graph) {
-            const elem = document.createElement('div');
-            elem.innerHTML = graph;
-            elem.className = 'mermaid';
-            elem.setAttribute('data-processed', 'true');
-            pre.parentElement.replaceChild(elem, pre);
-        }
-        mermaid.mermaidAPI.render('id' + i, e.textContent, replace);
-    }
-}
-
-if (document.readyState == 'interactive' || document.readyState == 'complete') {
-    mermaiding();
-}else{
-    document.addEventListener("DOMContentLoaded", mermaiding);
-}
+mermaid.initialize({
+    startOnLoad:true,
+    theme: 'default'
+});
+window.mermaid.init(undefined, document.querySelectorAll('.language-mermaid'));
