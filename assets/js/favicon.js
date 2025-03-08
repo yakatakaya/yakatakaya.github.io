ddc17d23abb04a349c935b2c8da05a9ea18cb7db
yakata <tagat92tagat@gmail.com>
2025-03-08 15:41:59 +0900

    favicon追加

---
+++ assets/js/favicon.js
@@ -0,0 +1,11 @@
+// favicon.js
+window.onload = function() {
+    console.log("Current directory:", window.location.href);
+    // faviconのリンクタグを作成
+    var link = document.createElement('link');
+    link.rel = 'icon';  // faviconの属性
+    link.href = './assets/images/favicon.ico';  // faviconファイルのパスを指定
+
+    // headにlinkタグを追加
+    document.head.appendChild(link);
+};
