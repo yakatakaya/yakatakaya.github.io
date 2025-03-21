ca47f72534140242bd2c989fcc8d278b56905baf
yakata <tagat92tagat@gmail.com>
2025-03-21 13:58:38 +0900

    Revert "adsense追加"
    This reverts commit 929b077ecc4339fe4f16d08076505ca8042e7482.

--- assets/js/ad.js
+++
@@ -1,5 +0,0 @@
-const ad_script = document.createElement('script');
-ad_script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6424991856628773';
-ad_script.async = true;
-ad_script.crossOrigin = 'anonymous';
-document.head.appendChild(ad_script);
